509f59525455fcc71fa4d107e60010e8d9d3bf7c
Joy <146790408+joy-totheworld@users.noreply.github.com>
2024-03-09 14:56:52 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ def main():
     """ Main program """
 
     for i in range(2):
-        can_turn_off_lights(f"GA4\\testfiles\\samples_subset\\test_medium_{i}.txt", "some")
+        curr = f"test_medium_{i}.txt"
+        print(curr)
+        print("actual:")
+        can_turn_off_lights(f"testfiles/samples_subset/test_medium_{i}.txt", "some")
+
+        expectedoutputfile = f"testfiles/samples_subset/test_medium_answer_{i}.txt"
+        with open(expectedoutputfile, 'r') as file:
+            expectedoutput1 = file.readline()
+            expectedoutput2 = file.readline()
+        print("")
+        print("expected output:")
+        print(expectedoutput1)
+        print(expectedoutput2)
+
+
+
     return 0
 
 if __name__ == "__main__":
